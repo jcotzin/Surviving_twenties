2f21f363bdbea3531740a82b759b58ed2f53515c
Jeremy Anderson <jande081@fiu.edu>
2016-06-28 14:35:13 -0400

    This program returns all the square root values of odd numbers ranging between 1 to 25.

---
+++ init/sqrt_odds.rb
@@ -0,0 +1,16 @@
+def sqrt_odds
+
+	i = 1
+	while (i <= 25) 
+		if(i%2 != 0)then
+			x = Math.sqrt(i)
+			puts "Odd has a square root which is #{x}"
+		else
+
+		end	
+		i = i + 1
+	end
+
+end
+
+sqrt_odds
